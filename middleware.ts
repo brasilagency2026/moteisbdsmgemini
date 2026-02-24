50255130cb84d5631dd71fa188a543ecd7faea0b
brasilagency2026 <brasilagency2026@gmail.com>
2026-02-24 13:42:28 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -21,3 +21,24 @@ export const config = {
     '/(api|trpc)(.*)',
   ],
 };
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+])
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth.protect()
+  }
+})
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+  // Ajoute ceci :
+  runtime: 'nodejs',
+}
